e0c28404ccf33b168d7ff5f2e00a1c95aee936a6
justinhochomo <chakh1@uci.edu>
2025-03-17 03:34:03 +0000

    fixing time out

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -3,10 +3,10 @@
 int main() {
     Graph G;
 
-    file_to_graph("small.txt", G); // You can change the filename
+    file_to_graph("small.txt", G);
 
     vector<int> previous;
-    vector<int> distances = dijkstra_shortest_path(G, 0, previous); // Source is vertex 0
+    vector<int> distances = dijkstra_shortest_path(G, 0, previous);
 
     for (int dest = 0; dest < G.numVertices; ++dest) {
         vector<int> path = extract_shortest_path(distances, previous, dest);
